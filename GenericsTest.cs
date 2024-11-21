16f32e4d8a41f3c4c0b5352c8be81d5fa33437ca
Alhasan Nofal <151967764+AlhasanNO@users.noreply.github.com>
2024-11-21 22:42:20 +0200

    Add files via upload

---
+++ GenericsTest.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class GenericsTest : MonoBehaviour
+{
+    void Start()
+    {
+        GameContainer<string> container = new GameContainer<string>();
+        container.SetItem("Healing Potion");
+
+        string savedItem = container.GetItem();
+        Debug.Log(savedItem);
+
+        string description = GameUtils.DescribeItem(savedItem);
+        Debug.Log(description);
+    }
+}
